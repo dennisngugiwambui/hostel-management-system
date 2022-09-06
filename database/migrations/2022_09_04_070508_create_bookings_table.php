d653690674440478cf0d0e6c679832ab81128417
dennisngugiwambui <112067611+dennisngugiwambui@users.noreply.github.com>
2022-09-06 06:26:23 +0300

    Add files via upload

---
+++ database/migrations/2022_09_04_070508_create_bookings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->String('hostel_id');
+            $table->string('room');
+            $table->string('price');
+            $table->string('people');
+            $table->string('phone');
+            $table->string('balance');
+            $table->string('date');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
